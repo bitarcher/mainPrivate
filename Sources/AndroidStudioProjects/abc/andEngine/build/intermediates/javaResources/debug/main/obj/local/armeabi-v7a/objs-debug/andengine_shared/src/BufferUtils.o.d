5c89cb3e7107d61357fbd9a1752edbb37eeafed9
Michel Strasser <mstrasser@bitarcher.com>
2014-12-31 17:45:19 +0100

    Ajout d'and engine compilé

---
+++ Sources/AndroidStudioProjects/abc/andEngine/build/intermediates/javaResources/debug/main/obj/local/armeabi-v7a/objs-debug/andengine_shared/src/BufferUtils.o.d
@@ -0,0 +1,74 @@
+/home/michel/mainPrivate/Sources/AndroidStudioProjects/abc/andEngine/src/main/obj/local/armeabi-v7a/objs-debug/andengine_shared/src/BufferUtils.o: \
+ /home/michel/mainPrivate/Sources/AndroidStudioProjects/abc/andEngine/src/main/jni/src/BufferUtils.cpp \
+ /home/michel/mainPrivate/Sources/AndroidStudioProjects/abc/andEngine/src/main/jni/src/BufferUtils.h \
+ /home/michel/AndroidLibs/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/jni.h \
+ /home/michel/AndroidLibs/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/sys/cdefs.h \
+ /home/michel/AndroidLibs/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/sys/cdefs_elf.h \
+ /home/michel/AndroidLibs/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/android/api-level.h \
+ /home/michel/AndroidLibs/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/stdio.h \
+ /home/michel/AndroidLibs/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/sys/types.h \
+ /home/michel/AndroidLibs/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/stdint.h \
+ /home/michel/AndroidLibs/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/sys/_types.h \
+ /home/michel/AndroidLibs/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/machine/_types.h \
+ /home/michel/AndroidLibs/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/linux/posix_types.h \
+ /home/michel/AndroidLibs/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/linux/stddef.h \
+ /home/michel/AndroidLibs/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/linux/compiler.h \
+ /home/michel/AndroidLibs/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/asm/posix_types.h \
+ /home/michel/AndroidLibs/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/asm/types.h \
+ /home/michel/AndroidLibs/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/linux/types.h \
+ /home/michel/AndroidLibs/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/machine/kernel.h \
+ /home/michel/AndroidLibs/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/sys/sysmacros.h \
+ /home/michel/AndroidLibs/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/stdlib.h \
+ /home/michel/AndroidLibs/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/string.h \
+ /home/michel/AndroidLibs/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/malloc.h \
+ /home/michel/AndroidLibs/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/alloca.h \
+ /home/michel/AndroidLibs/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/strings.h \
+ /home/michel/AndroidLibs/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/memory.h
+
+/home/michel/mainPrivate/Sources/AndroidStudioProjects/abc/andEngine/src/main/jni/src/BufferUtils.h:
+
+/home/michel/AndroidLibs/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/jni.h:
+
+/home/michel/AndroidLibs/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/sys/cdefs.h:
+
+/home/michel/AndroidLibs/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/home/michel/AndroidLibs/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/android/api-level.h:
+
+/home/michel/AndroidLibs/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/stdio.h:
+
+/home/michel/AndroidLibs/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/sys/types.h:
+
+/home/michel/AndroidLibs/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/stdint.h:
+
+/home/michel/AndroidLibs/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/sys/_types.h:
+
+/home/michel/AndroidLibs/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/machine/_types.h:
+
+/home/michel/AndroidLibs/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/linux/posix_types.h:
+
+/home/michel/AndroidLibs/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/linux/stddef.h:
+
+/home/michel/AndroidLibs/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/linux/compiler.h:
+
+/home/michel/AndroidLibs/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/asm/posix_types.h:
+
+/home/michel/AndroidLibs/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/asm/types.h:
+
+/home/michel/AndroidLibs/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/linux/types.h:
+
+/home/michel/AndroidLibs/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/machine/kernel.h:
+
+/home/michel/AndroidLibs/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/sys/sysmacros.h:
+
+/home/michel/AndroidLibs/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/stdlib.h:
+
+/home/michel/AndroidLibs/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/string.h:
+
+/home/michel/AndroidLibs/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/malloc.h:
+
+/home/michel/AndroidLibs/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/alloca.h:
+
+/home/michel/AndroidLibs/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/strings.h:
+
+/home/michel/AndroidLibs/android-ndk-r10d/platforms/android-19/arch-arm/usr/include/memory.h:
---
+++ Sources/AndroidStudioProjects/abc/app/src/androidTest/java/com/bitarcher/abc/WorkingWithSVG.java
@@ -0,0 +1,7 @@
+package com.bitarcher.abc;
+
+/**
+ * Created by michel on 31/12/14.
+ */
+public class WorkingWithSVG {
+}
