73028d110b37c3b4444f9af6407fc1c76a4c840d
Michel Strasser <mstrasser@bitarcher.com>
2015-01-05 13:04:36 +0100

    Jumping to branch https://github.com/RealMayo/AndEngineSVGTextureRegionExtension like it has been suggested in forum http://www.andengine.org/forums/gles2/build-error-with-anchor-center-and-svg-extension-t13001.html

--- Sources/AndroidStudioProjects/abc/app/src/main/com/bitarcher/abc/bll/Dinosaur.java
+++
@@ -1,9 +0,0 @@
-package com.bitarcher.abc.bll;
-
-import com.bitarcher.abc.interfaces.bll.IDinosaur;
-
-/**
- * Created by michel on 19/12/14.
- */
-public class Dinosaur extends APictureBase implements IDinosaur {
-}
--- Sources/AndroidStudioProjects/abc/app/src/main/com/bitarcher/abc/bll/Idiogram.java
+++
@@ -1,9 +0,0 @@
-package com.bitarcher.abc.bll;
-
-import com.bitarcher.abc.interfaces.bll.IIdiogram;
-
-/**
- * Created by michel on 19/12/14.
- */
-public class Idiogram extends APictureBase implements IIdiogram {
-}
