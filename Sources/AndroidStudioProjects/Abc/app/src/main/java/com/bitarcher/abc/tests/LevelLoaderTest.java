48aa876c4b63688245237222d87dabdfce7fd8d2
Michel Strasser <mstrasser@bitarcher.com>
2015-01-20 16:46:05 +0100

    Introducing unit testing

---
+++ Sources/AndroidStudioProjects/Abc/app/src/main/java/com/bitarcher/abc/tests/LevelLoaderTest.java
@@ -0,0 +1,69 @@
+package com.bitarcher.abc.tests;
+
+import android.app.Application;
+import android.test.InstrumentationTestCase;
+
+import com.bitarcher.abc.bll.xml.ro.levels.Node;
+
+import org.w3c.dom.Attr;
+import org.w3c.dom.CDATASection;
+import org.w3c.dom.Comment;
+import org.w3c.dom.DOMConfiguration;
+import org.w3c.dom.DOMException;
+import org.w3c.dom.DOMImplementation;
+import org.w3c.dom.Document;
+import org.w3c.dom.DocumentFragment;
+import org.w3c.dom.DocumentType;
+import org.w3c.dom.Element;
+import org.w3c.dom.EntityReference;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.ProcessingInstruction;
+import org.w3c.dom.Text;
+import org.w3c.dom.UserDataHandler;
+
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+/**
+ * Created by michel on 20/01/15.
+ */
+public class LevelLoaderTest extends InstrumentationTestCase {
+
+    private Application m_app;
+
+
+    public void testFailed() throws Exception{
+        final int expected = 1;
+        final int reality = 5;
+        assertEquals(expected, reality);
+    }
+
+
+    public void testLoading() throws Exception
+    {
+        m_app = new Application();
+        //call to system under test to load m_app using
+        //a sample xml file
+
+
+
+        Node rootNode =null;
+        try {
+            DocumentBuilderFactory DOMfactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder DOMbuilder = DOMfactory.newDocumentBuilder();
+
+            Document doc = DOMbuilder.parse(this.getInstrumentation().getContext().getResources().getAssets().open("Levels.xml"));
+
+            Node node = new Node(null);
+            node.fromXml(doc.getDocumentElement());
+
+
+        } catch (ParserConfigurationException e) {
+
+            e.printStackTrace();
+        }
+    }
+}
