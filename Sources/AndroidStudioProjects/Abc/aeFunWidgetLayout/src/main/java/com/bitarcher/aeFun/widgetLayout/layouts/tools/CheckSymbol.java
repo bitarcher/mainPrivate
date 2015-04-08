0fb261e89c32ad98705c5461eb9e3e6083461888
Michel Strasser <mstrasser@bitarcher.com>
2015-04-08 15:57:44 +0200

    check symbol -> geometry primitives

--- Sources/AndroidStudioProjects/Abc/aeFunWidgetLayout/src/main/java/com/bitarcher/aeFun/widgetLayout/layouts/tools/CheckSymbol.java
+++ Sources/AndroidStudioProjects/Abc/aeFunWidgetLayout/src/main/java/com/bitarcher/aeFun/widgetLayout/layouts/tools/CheckSymbol.java
@@ -9,45 +9,26 @@ package com.bitarcher.aeFun.widgetLayout.layouts.tools;
 import com.bitarcher.aeFun.interfaces.gui.theme.ITheme;
 import com.bitarcher.aeFun.interfaces.gui.widgets.Tools.IEnableSetterEntity;
 
-import org.andengine.entity.primitive.DrawMode;
-import org.andengine.entity.primitive.Mesh;
+import org.andengine.entity.Entity;
 import org.andengine.util.adt.color.Color;
 
 /**
  * Created by michel on 08/04/15.
  */
-public class CheckSymbol extends Mesh implements IEnableSetterEntity {
+public class CheckSymbol extends Entity implements IEnableSetterEntity {
 
     CheckButtonClickableEntity checkButtonClickableEntity;
+    com.bitarcher.aeFun.geometry.primitives.CheckSymbol checkSymbol;
 
     public CheckSymbol(CheckButtonClickableEntity checkButtonClickableEntity, float x, float y, float width, float height) {
-        super(x, y, width, height, CheckSymbol.getCoordinates(width, height), 6, DrawMode.TRIANGLE_STRIP, checkButtonClickableEntity.checkableLayout.getWidget().getTheme().getThemeManager().getResourceManager().getEngine().getVertexBufferObjectManager()); // dummy init
+        super(x, y, width, height);
 
         this.checkButtonClickableEntity = checkButtonClickableEntity;
+
+        this.checkSymbol = new com.bitarcher.aeFun.geometry.primitives.CheckSymbol(width / 2, height / 2, width, height,
+            checkButtonClickableEntity.checkableLayout.getWidget().getTheme().getThemeManager().getResourceManager().getEngine().getVertexBufferObjectManager());
         this.setColor();
-    }
-
-    static float[] getCoordinates(float width, float height)
-    {
-        float UNUSED = 0;
-
-        // coordinates for triangle_strip
-        float[] retval = new float[]{
-            // p0
-            0.05f * width, 0.6f * height, UNUSED,
-            // p1
-            0, 0.5f * height, UNUSED,
-            // p2
-            0.4f * width, 0, UNUSED,
-            // p3
-            0.4f * width, 0.15f * height, UNUSED,
-            // p4
-            1f * width, 0.95f * height, UNUSED,
-            // p5
-            0.9f * width, 1 * height, UNUSED
-        };
-
-        return retval;
+        this.attachChild(this.checkSymbol);
     }
 
     void setColor()
@@ -64,7 +45,7 @@ public class CheckSymbol extends Mesh implements IEnableSetterEntity {
             color = theme.getWidgetSections().getCheckButtonSection().getDisabledColor2();
         }
 
-        this.setColor(color);
+        this.checkSymbol.setColor(color);
     }
 
     @Override
